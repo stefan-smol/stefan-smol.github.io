22db53de51289d72301ec8b56867891228c2171f
stefan-smol <stefans3@my.yorku.ca>
2025-03-10 00:02:16 -0400

    .

---
+++ main.js
@@ -0,0 +1,34 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+const themeToggleButton = document.querySelector("#theme-toggle");
+
+function toggleTheme() {
+  const currentTheme =
+    document.documentElement.getAttribute("data-theme") || "light";
+  const newTheme = currentTheme === "light" ? "dark" : "light";
+
+  document.documentElement.setAttribute("data-theme", newTheme);
+  localStorage.setItem("theme", newTheme);
+}
+
+const storedTheme = localStorage.getItem("theme") || "light";
+document.documentElement.setAttribute("data-theme", storedTheme);
+
+themeToggleButton.addEventListener("click", toggleTheme);
